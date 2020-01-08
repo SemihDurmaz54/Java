0c9e63f6945d744dec4de6ff96164a2e075ecfde
Semih Durmaz <51100038+SemihDurmaz54@users.noreply.github.com>
2020-01-08 11:12:52 +0300

    Update Login.java

--- muhasebeProjem/src/muhasebeprojesi/Login.java
+++ muhasebeProjem/src/muhasebeprojesi/Login.java
@@ -1,8 +1,7 @@
 package muhasebeprojesi;
 
-import java.awt.BorderLayout; //
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
